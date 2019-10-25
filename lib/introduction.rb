f6ce3acb1928b1854d6e6f111c92c019775e722a
vy vo <tuonggvy.vo@gmail.com>
2019-10-25 17:54:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,4 +3,7 @@ def introduction(name)
 end
   
   def introduction_with_language(name,language)
-    puts "Hi, M"
+    puts "Hi, my name is #{name} and I am learning to program 
+    in #{language}.\n"
+  end
+  
